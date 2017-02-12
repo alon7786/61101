3b66323a828258ef50660225e7788b64fa5bf3af
alon7786 <alon7786@gmail.com>
2017-02-12 12:55:18 +0200

    update
    merge replaced with merge_arrays

--- selected.c
+++ selected.c
@@ -138,24 +138,23 @@ int bin_search_last( int key, int *a, int n )
 
 void merge_sort( int *a, int first, int last );
 
-void merge( int *a, int first, int middle, int last )
+int *merge_arrays( int *a, int *b, int size_of_a, int size_of_b )
 {
-	int left_index = first, right_index = middle + 1, temp_index = 0;
-	int *temp = ( int* )malloc( ( last - first + 1 ) * sizeof( int ) );
-	while( ( left_index <= middle ) && ( right_index <= last ) )
+	int *c = ( int* )malloc( ( size_of_a + size_of_b ) * sizeof( int ) );
+	int index_of_a = 0, index_of_b = 0, index_of_c = 0;
+	while( ( index_of_a < size_of_a )
+		&& ( index_of_b < size_of_b ) )
 	{
-		if( a[ left_index ] < a[ right_index ] )
-			temp[ tmep_index++ ] = a[ left_index++ ];
+		if( a[ index_of_a ] < b[ index_of_b ] )
+			c[ index_of_c++ ] = a[ index_of_a++ ];
 		else
-			temp[ tmep_index++ ] = a[ right_index++ ];
+			c[ index_of_c++ ] = b[ index_of_b++ ];
 	}
-	while( right_index <= last )
-		temp[ tmep_index++ ] = a[ right_index++ ];
-	while( left_index <= middle )
-		temp[ tmep_index++ ] = a[ left_index++ ];
-	for( left_index = first, tmep_index = 0; left_index <= last; left_index++, tmep_index++ )
-		a[ left_index ] = temp[ tmep_index ];
-	free( temp );
+	while( index_of_a < size_of_a )
+		c[ index_of_c++ ] = a[ index_of_a++ ];
+	while( index_of_b < size_of_b )
+		c[ index_of_c++ ] = b[ index_of_b++ ];
+	return c;
 }
 
 // lecture 14
